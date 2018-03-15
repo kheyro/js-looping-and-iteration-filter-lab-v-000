d202b8f53ee6efc9daccec1d532b3ad78f5c3f4c
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 21:35:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 function findMatching(arr, name) {
   return arr.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase() })
 }
+
+function fuzzyMatch(arr, name) {
+  return arr.filter(function (driver) { return driver.slice(0, name.length) === name })
+}
