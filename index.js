ce331b2f406cc8013e141ff02ef1fa1542cf5b01
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 21:31:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 
 function findMatching(arr, name) {
-  arr.filter(function (driver) { return driver === name })
+  arr.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase() })
 }
